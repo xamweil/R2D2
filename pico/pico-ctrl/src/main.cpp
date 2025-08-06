c2b4b1132f6aa5fd02badcf80086aef00dfdd751
Maximilian Schüle <max.schuele@mailbox.org>
2025-08-06 23:43:29 +0200

    pins

--- pico/pico-ctrl/src/main.cpp
+++ pico/pico-ctrl/src/main.cpp
@@ -1,15 +1,54 @@
+#include "SerialUSB.h"
 #include <Arduino.h>
 
-const int LED_PIN = 22;
+const int ENA_PIN = 6;
+const int DIR_PIN = 4;
+const int PUL_PIN = 5;
 
-const int ENA_PIN = 22;
-const int DIR_PIN = 20;
-const int PUL_PIN = 21;
+const int LED_PIN = 25;
+
+/**
+ * 6 = ENA
+ * 4 = DIR
+ * 5 = PUL
+ */
 
 void setup() {
   pinMode(ENA_PIN, OUTPUT);
   pinMode(DIR_PIN, OUTPUT);
   pinMode(PUL_PIN, OUTPUT);
+
+  pinMode(LED_PIN, OUTPUT);
+
+  digitalWrite(ENA_PIN, LOW);
+
+  Serial.begin(115200);
 }
 
-void loop() { sample_pwm(); }
+int Index;
+
+void loop() {
+  digitalWrite(DIR_PIN, HIGH);
+  digitalWrite(LED_PIN, HIGH);
+
+  Serial.println("FIRST LOOP\n");
+  for (Index = 0; Index < 5000; Index++) {
+    digitalWrite(PUL_PIN, HIGH);
+    delayMicroseconds(500);
+    digitalWrite(PUL_PIN, LOW);
+    delayMicroseconds(500);
+  }
+  delay(1000);
+
+  digitalWrite(LED_PIN, LOW);
+  digitalWrite(DIR_PIN, LOW);
+
+  Serial.print("SECOND LOOP\n");
+  for (Index = 0; Index < 5000; Index++) {
+    digitalWrite(PUL_PIN, HIGH);
+    delayMicroseconds(500);
+    digitalWrite(PUL_PIN, LOW);
+    delayMicroseconds(500);
+  }
+  delay(1000);
+}
